e866b8496c0e071a57d2ec5bba066b2786eaf9a9
linjinghui <852530363@qq.com>
2018-04-11 22:37:41 +0800

    箭头图标调整

--- example/demo_cicon.vue
+++ example/demo_cicon.vue
@@ -1,10 +1,10 @@
 <template>
-  <div style="padding: 50px;">
+  <div style="padding: 50px;color: #fff;">
     <i class="cicon-sub-chr-cpt" style="background-color: green;"></i> 
     <i class="cicon-plus-cpt-chr" style="background-color: blue;"></i> 
     <i class="cicon-cross-cpt" style="background-color: yellow;"></i>
-    <i class="cicon-cross-2-cpt"></i>
-    <i class="cicon-cross-cpt-crle"></i>
+    <i class="cicon-cross-crle-chr" style="background-color: #999;"></i>
+    <i class="cicon-cross-crle-cbdr"></i>
     <i class="cicon-tick" style="background-color: green;"></i> 
     <i class="cicon-search-cpt-chr" style="background-color: blue;"></i> 
     <i class="cicon-search-2-cpt" style="background-color: yellow;"></i>
@@ -13,13 +13,14 @@
     <i class="cicon-triangle-left" style="background-color: yellow;"></i> 
     <i class="cicon-triangle-top" style="background-color: green;"></i> 
     <i class="cicon-triangle-bottom" style="background-color: blue;"></i> 
-    <i class="cicon-arrow-right" style="background-color: yellow;"></i> 
+    <i class="cicon-arrow-right-cbdr-crle" style="background-color: yellow;"></i> 
     <i class="cicon-arrow-left" style="background-color: green;"></i> 
     <i class="cicon-arrow-top" style="background-color: blue;"></i> 
     <i class="cicon-arrow-bottom" style="background-color: yellow;"></i> 
     <i class="cicon-loading move-loop1" style="background-color: green;">
       <span></span><span></span><span></span>
     </i> 
+    <i class="cicon-exclamation-crle-cbdr" style=""></i> 
   </div>
 </template>
 
