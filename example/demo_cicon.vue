35be4b5063904b85772a318c102ae7d4a30b2d5d
linjinghui <852530363@qq.com>
2018-04-24 19:12:16 +0800

    新增一个加载动画图标

--- example/demo_cicon.vue
+++ example/demo_cicon.vue
@@ -1,26 +1,27 @@
 <template>
   <div style="padding: 50px;color: #fff;">
-    <i class="cicon-sub-chr-cpt" style="background-color: green;"></i> 
-    <i class="cicon-plus-cpt-chr" style="background-color: blue;"></i> 
+    <i class="cicon-sub-chr-cpt" style="background-color: green;"></i>
+    <i class="cicon-plus-cpt-chr" style="background-color: blue;"></i>
     <i class="cicon-cross-cpt" style="background-color: yellow;"></i>
     <i class="cicon-cross-crle-chr" style="background-color: #999;"></i>
     <i class="cicon-cross-crle-cbdr"></i>
-    <i class="cicon-tick" style="background-color: green;"></i> 
-    <i class="cicon-search-cpt-chr" style="background-color: blue;"></i> 
+    <i class="cicon-tick" style="background-color: green;"></i>
+    <i class="cicon-search-cpt-chr" style="background-color: blue;"></i>
     <i class="cicon-search-2-cpt" style="background-color: yellow;"></i>
-    <i class="cicon-play" style="background-color: green;"></i> 
-    <i class="cicon-triangle-right" style="background-color: blue;"></i> 
-    <i class="cicon-triangle-left" style="background-color: yellow;"></i> 
-    <i class="cicon-triangle-top" style="background-color: green;"></i> 
-    <i class="cicon-triangle-bottom" style="background-color: blue;"></i> 
-    <i class="cicon-arrow-right-cbdr-crle" style="background-color: yellow;"></i> 
-    <i class="cicon-arrow-left" style="background-color: green;"></i> 
-    <i class="cicon-arrow-top" style="background-color: blue;"></i> 
-    <i class="cicon-arrow-bottom" style="background-color: yellow;"></i> 
+    <i class="cicon-play" style="background-color: green;"></i>
+    <i class="cicon-triangle-right" style="background-color: blue;"></i>
+    <i class="cicon-triangle-left" style="background-color: yellow;"></i>
+    <i class="cicon-triangle-top" style="background-color: green;"></i>
+    <i class="cicon-triangle-bottom" style="background-color: blue;"></i>
+    <i class="cicon-arrow-right-cbdr-crle" style="background-color: yellow;"></i>
+    <i class="cicon-arrow-left" style="background-color: green;"></i>
+    <i class="cicon-arrow-top" style="background-color: blue;"></i>
+    <i class="cicon-arrow-bottom" style="background-color: yellow;"></i>
     <i class="cicon-loading move-loop1" style="background-color: green;">
       <span></span><span></span><span></span>
-    </i> 
-    <i class="cicon-exclamation-crle-cbdr" style=""></i> 
+    </i>
+    <i class="cicon-exclamation-crle-cbdr" style=""></i>
+    <i class="cicon-circle-loading move-loop1" style="background-color: blue;color: #fff;"></i>
   </div>
 </template>
 
@@ -34,14 +35,14 @@ export default {
     };
   },
   components: {
-    // 
+    //
   },
   methods: {
-    // 
+    //
   }
 };
 </script>
 
 <style scoped>
-  
+
 </style>
