1bd883d277e5495b70346ebcc80431c42c0c8c16
linjinghui <852530363@qq.com>
2018-04-28 13:58:08 +0800

    实现rangemenu功能

--- example/demo_range_menu.vue
+++ example/demo_range_menu.vue
@@ -1,5 +1,13 @@
 <template>
-  <cmp-range-menu></cmp-range-menu>
+  <!--  -->
+  <div>
+    <!-- 默认方式 -->
+    <cmp-range-menu v-model="list" @add="fun_add"></cmp-range-menu>
+    <!-- 如需自定义显示内容，请使用以下方式 -->
+    <cmp-range-menu v-model="list2"  @add="fun_add2">
+      <template slot-scope="props">{{ props.item.name +  props.item.age}}</template>
+    </cmp-range-menu>
+  </div>
 </template>
 
 <script>
@@ -9,17 +17,59 @@ export default {
   name: 'demoRangeMenu',
   data: function () {
     return {
-      //
+      list: [
+        '淡淡的',
+        '嗖嗖嗖',
+        'vvvv',
+        '密麻麻'
+      ],
+      list2: [
+        {
+          name: 'name1',
+          age: 11
+        },
+        {
+          name: 'name2',
+          age: 12
+        },
+        {
+          name: 'name3',
+          age: 13
+        },
+        {
+          name: 'name4',
+          age: 14
+        },
+        {
+          name: 'name5',
+          age: 15
+        }
+      ]
     };
   },
   components: {
     'cmpRangeMenu': RangeMenu
   },
   watch: {
-    //
+    list: function (val) {
+      console.log('========watch list========');
+      console.log(val);
+    },
+    list2: function (val) {
+      console.log('========watch list2========');
+      console.log(val);
+    }
   },
   methods: {
-    //
+    fun_add: function () {
+      this.list.push('item' + this.list.length);
+    },
+    fun_add2: function () {
+      this.list2.push({
+        'name': 'name' + this.list2.length,
+        'age': this.list2.length
+      });
+    }
   }
 };
 </script>
--- packages/rangeMenu/src/main.vue
+++ packages/rangeMenu/src/main.vue
@@ -1,22 +1,44 @@
 <!--
 功能介绍：
-1、
+1、可通过模版自定义显示内容
+例如 <template slot-scope="props">{{ props.item.name +  props.item.age}}</template>
  -->
 
 <template>
-  <div>RangeMenu</div>
+  <vperfect-scrollbar class="rangeMenu" :settings="settings">
+    <a v-for="(item, index) in value" :key="item.id">
+      <label>
+        <slot :item="item">{{item}}</slot>
+      </label>
+      <i class="cicon-cross-cpt-crle-chr cross" @click="clk_del(index)"></i>
+    </a>
+    <a class="add" @click="clk_add">添加</a>
+  </vperfect-scrollbar>
 </template>
 
 <script type="text/babel">
+  import VuePerfectScrollbar from 'vue-perfect-scrollbar';
+
   export default {
     name: 'RangeMenu',
+    components: {
+      'vperfect-scrollbar': VuePerfectScrollbar
+    },
     data: function () {
       return {
-        id: 'btn_' + new Date().getTime() + parseInt(Math.random() * 100)
+        id: 'btn_' + new Date().getTime() + parseInt(Math.random() * 100),
+        // 滚动速度，默认1
+        settings: {
+          wheelSpeed: 0.5
+        }
       };
     },
     props: {
-      //
+      value: {
+        default: function () {
+          return [];
+        }
+      }
     },
     watch: {
       //
@@ -31,11 +53,92 @@
       //
     },
     methods: {
-      //
+      emtVal: function (val) {
+        this.$emit('input', val);
+      },
+      clk_add: function () {
+        this.$emit('add');
+      },
+      clk_del: function (index) {
+        let data = '';
+
+        try {
+          data = JSON.parse(JSON.stringify(this.value));
+          data.splice(index, 1);
+          this.emtVal(data);
+        } catch (e) {
+          //
+        }
+      }
     }
   };
 </script>
 
 <style scoped lang="scss">
-  //
+  $padding: 5px;
+
+  .rangeMenu {
+    max-height: 210px;
+    padding-top: $padding;
+    padding-left: $padding;
+    border: solid 1px #ddd;
+    border-radius: 4px;
+    color: #333;
+    background-color: #fff;
+    overflow: auto;
+    font-size: 14px;
+    user-select: none;
+
+    a {
+      float: left;
+      margin-right: $padding;
+      margin-bottom: $padding;
+      height: 24px;
+      line-height: 24px;
+      padding-left: $padding;
+      padding-right: $padding;
+      border-radius: 4px;
+      color: #909399;
+      background-color: #f0f2f5;
+      cursor: default;
+
+      label {
+        display: inline-block;
+        max-width: 84px;
+        height: 100%;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+      }
+
+      .cross {
+        color: inheirt;
+        font-size: 14px;
+        vertical-align: 4px;
+      }
+
+      .cross:hover {
+        color: #fff;
+        background-color: #c0c4cc;
+      }
+    }
+
+    a.add {
+      color: #0079ff;
+      background-color: transparent;
+      cursor: pointer;
+    }
+
+    a.add:hover {
+      color: #45abff;
+    }
+
+    a.add:active {
+      color: #1072ba;
+    }
+
+    a:not(.add):hover {
+      background-color: #e6e6e6;
+    }
+  }
 </style>
