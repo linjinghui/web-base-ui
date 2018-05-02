eb99fb9d38873db52bd97543328102320f675e25
linjinghui <852530363@qq.com>
2018-05-02 16:54:17 +0800

    checkbox禁用后，不允许再设置值

--- packages/checkbox/src/main.vue
+++ packages/checkbox/src/main.vue
@@ -34,6 +34,13 @@
         type: Function
       }
     },
+    watch: {
+      value: function (val) {
+        if (val && (this.disabled + '') === 'true') {
+          this.$emit('input', false);
+        }
+      }
+    },
     computed: {
       _style: function () {
         return {
