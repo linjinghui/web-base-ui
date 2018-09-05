35b0906274fa5fb034f74f2690d0739111c85e3a
linjinghui <852530363@qq.com>
2018-09-05 18:58:03 +0800

    fix

--- example/demo_aside_nav.vue
+++ example/demo_aside_nav.vue
@@ -1,15 +1,15 @@
 <template>
   <div style="width: 220px;height: 400px;border:solid 1px;background-color: #222d32;overflow-y: auto;">
     <ul class="aside-nav">
-      <li v-for="(item1,index) in navData" :key="'lay1'+index">
-        <a :class="{'active':activeIndex_1===item1.id, 'open': openIndex_1===item1.id}" @click="clkItem([item1])">
+      <li v-for="(item1,index) in navOption.navData" :key="'lay1'+index">
+        <a :class="{'active':navOption.activeIndex_1===item1.id, 'open': navOption.openIndex_1===item1.id}" @click="clkNavItem([item1])">
           <i class="i-l">&nbsp;</i>
           <span>{{item1.name}}</span>
           <i class="i-r arrow" v-if="item1.children&&item1.children.length>0"><</i>
         </a>
-        <ul :style="{maxHeight:openIndex_1===item1.id?item1.children.length*34+'px':'0px'}">
+        <ul :style="{maxHeight:navOption.openIndex_1===item1.id?item1.children.length*34+'px':'0px'}">
           <li v-for="(item2,index) in item1.children" :key="'lay2'+index">
-            <a :class="{'active':activeIndex_2===item2.id}" @click="clkItem([item1,item2])">
+            <a :class="{'active':navOption.activeIndex_2===item2.id}" @click="clkNavItem([item1,item2])">
               <i class="i-l">&nbsp;</i>
               <span>{{item2.name}}</span>
               <i class="i-r arrow" v-if="item2.children&&item2.children.length>0"><</i>
@@ -28,96 +28,98 @@ export default {
   name: 'demoButton',
   data: function () {
     return {
-      openIndex_1: '',
-      activeIndex_1: '',
-      activeIndex_2: '',
-      navData: [
-        // {
-        //   id: '', 
-        //   name: '',
-        //   icon: '',
-        //   url: '',
-        //   children: []
-        // },
-        {
-          id: '1',
-          name: '主体审核',
-          icon: '',
-          url: '',
-          children: [
-            {
-              id: '1-1',
-              name: '待审核',
-              icon: '',
-              url: 'https://www.baidu.com?t=1',
-              children: []
-            },
-            {
-              id: '1-2',
-              name: '历史审核',
-              icon: '',
-              url: 'https://www.baidu.com?t=2',
-              children: []
-            }
-          ]
-        },
-        {
-          id: '2',
-          name: '主体档案巡查',
-          icon: '',
-          children: [
-            {
-              id: '2-1',
-              name: '主体巡查',
-              icon: '',
-              url: '',
-              children: []
-            },
-            {
-              id: '2-2',
-              name: '预警查询',
-              icon: '',
-              url: '',
-              children: []
-            },
-            {
-              id: '2-3',
-              name: 'GIS展示',
-              icon: '',
-              url: '',
-              children: []
-            }
-          ]
-        },
-        {
-          id: '3',
-          name: '系统管理',
-          icon: '',
-          children: [
-            {
-              id: '3-1',
-              name: '组织机构管理',
-              icon: '',
-              url: 'https://www.baidu.com?t=3',
-              children: []
-            },
-            {
-              id: '3-2',
-              name: '角色管理',
-              icon: '',
-              url: 'https://www.baidu.com?t=4',
-              children: []
-            },
-            {
-              id: '3-3',
-              name: '角色及权限管理',
-              icon: '',
-              url: 'https://www.baidu.com?t=5',
-              children: []
-            }
-          ]
-        }
-      ]
+      navOption: {
+        openIndex_1: '',
+        activeIndex_1: '',
+        activeIndex_2: '',
+        navData: [
+          // {
+          //   id: '', 
+          //   name: '',
+          //   icon: '',
+          //   url: '',
+          //   children: []
+          // },
+          {
+            id: '1',
+            name: '主体审核',
+            icon: '',
+            url: '',
+            children: [
+              {
+                id: '1-1',
+                name: '待审核',
+                icon: '',
+                url: 'https://www.baidu.com?t=1',
+                children: []
+              },
+              {
+                id: '1-2',
+                name: '历史审核',
+                icon: '',
+                url: 'https://www.baidu.com?t=2',
+                children: []
+              }
+            ]
+          },
+          {
+            id: '2',
+            name: '主体档案巡查',
+            icon: '',
+            children: [
+              {
+                id: '2-1',
+                name: '主体巡查',
+                icon: '',
+                url: '',
+                children: []
+              },
+              {
+                id: '2-2',
+                name: '预警查询',
+                icon: '',
+                url: '',
+                children: []
+              },
+              {
+                id: '2-3',
+                name: 'GIS展示',
+                icon: '',
+                url: '',
+                children: []
+              }
+            ]
+          },
+          {
+            id: '3',
+            name: '系统管理',
+            icon: '',
+            children: [
+              {
+                id: '3-1',
+                name: '组织机构管理',
+                icon: '',
+                url: 'https://www.baidu.com?t=3',
+                children: []
+              },
+              {
+                id: '3-2',
+                name: '角色管理',
+                icon: '',
+                url: 'https://www.baidu.com?t=4',
+                children: []
+              },
+              {
+                id: '3-3',
+                name: '角色及权限管理',
+                icon: '',
+                url: 'https://www.baidu.com?t=5',
+                children: []
+              }
+            ]
+          }
+        ]
+      }
     };
   },
   components: {
@@ -127,7 +129,7 @@ export default {
     //
   },
   methods: {
-    clkItem: function (arr) {
+    clkNavItem: function (arr) {
       var current = arr[arr.length - 1];
       var first = arr.length > 1 ? arr[0] : {};
 
@@ -136,27 +138,13 @@ export default {
         // step1 - 跳转
         console.log('跳转：' + current.url);
         // step2 - 激活当前
-        this.activeIndex_2 = current.id;
+        this.navOption.activeIndex_2 = current.id;
         // step3 - 激活最上层
-        this.activeIndex_1 = first.id;
+        this.navOption.activeIndex_1 = first.id;
       } else if (current.children && current.children.length > 0) {
         // 开启子节点
-        this.openIndex_1 = this.openIndex_1 === current.id ? '' : current.id;
-        this.activeIndex_2 = '';
-      }
-    },
-    clkItem_bf: function (info) {
-      console.log(info.show);
-      var _this = this;
-
-      this.$set(info, 'show', !info.show);
-      // 关闭其他
-      if (info.show) {
-        this.navData.forEach(element => {
-          if (element.name !== info.name) {
-            _this.$set(element, 'show', false);
-          }
-        });
+        this.navOption.openIndex_1 = this.navOption.openIndex_1 === current.id ? '' : current.id;
+        this.navOption.activeIndex_2 = '';
       }
     }
   }
@@ -194,7 +182,7 @@ export default {
   .aside-nav li > a > .arrow {
     transition: transform .4s ease;
   }
-  .aside-nav li > a.show > .arrow {
+  .aside-nav li > a.open > .arrow {
     transform: rotate(-90deg);
   }
     
