283d342f85fd02e1008012d88333a624f7a06e2c
linjinghui <852530363@qq.com>
2018-12-04 23:39:12 +0800

    播放器功能实现

--- example/demo_video.vue
+++ example/demo_video.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <cmp-video v-model="optVideo.show"></cmp-video>
+    <cmp-video v-model="optVideo.show" v-bind="optVideo"></cmp-video>
     <button @click="toggle">显示、隐藏</button>
+    <button @click="clkNew">设置新视频</button>
   </div>
 </template>
 
@@ -13,7 +14,22 @@ export default {
   data: function () {
     return {
       optVideo: {
-        show: ''
+        show: '',
+        model: false,
+        title: '华为主题曲',
+        opt: {
+          // 封面图片地址
+          poster: 'http://pic1.nipic.com/2009-01-07/20091713417344_2.jpg', 
+          // 是否自动播放
+          autoplay: false, 
+          // 是否循环播放
+          loop: false, 
+          // 默认需要跳转的时间
+          seek: 0,
+          video: [
+            ['http://img.ksbbs.com/asset/Mon_1703/d30e02a5626c066.mp4', 'video/mp4', '英文标清', 0]
+          ]
+        }
       }
     };
   },
@@ -26,6 +42,11 @@ export default {
   methods: {
     toggle: function () {
       this.optVideo.show = !this.optVideo.show;
+    },
+    clkNew: function () {
+      this.optVideo.opt.video = [
+        ['http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4', 'video/mp4', '中文标清', 0]
+      ];
     }
   }
 };
--- packages/video/src/main.vue
+++ packages/video/src/main.vue
@@ -6,7 +6,13 @@ https://github.com/niandeng-ckplayer/chplayer
 
 <template>
   <transition name="slide-fade">
-    <div :id="id" class="wrap-video" v-if="value!==''" v-show="value">VIDEO</div>
+    <div class="wrap-video" :style="{'z-index': zIndex + 1}" v-if="value!==''" v-show="value">
+      <header>
+        <span>{{title}}</span>
+        <i class="cicon-cross-cpt-chr" @click="clkClose"></i>
+      </header>
+      <section :id="id"></section>
+    </div>
   </transition>
 </template>
 
@@ -21,30 +27,32 @@ https://github.com/niandeng-ckplayer/chplayer
       return {
         id: id,
         player: '',
+        zIndex: 1000,
         option: {
           // 容器的ID或className
           container: '#' + id,
           variable: 'player',
+          logo: ' ',
           // 默认音量
           volume: 0.6,
           // 封面图片地址
-          poster: '../lib/screenshot/wdm.jpg', 
+          poster: '', 
           // 是否自动播放
           autoplay: false, 
           // 是否循环播放
-          loop: true, 
+          loop: false, 
           // 是否是直播
           live: false, 
           // 当播放器加载后执行的函数
-          loaded: 'loadedHandler', 
+          // loaded: 'loadedHandler', 
           // 默认需要跳转的时间
           seek: 0, 
           // 在flashplayer情况下是否需要支持拖动，拖动的属性是什么
-          drag: '', 
+          // drag: '', 
           // 前一集按钮动作
-          front: 'frontFun', 
+          // front: 'frontFun', 
           // 下一集按钮动作
-          next: 'nextFun', 
+          // next: 'nextFun', 
           // flashplayer: true, // 强制使用flashplayer
           // html5m3u8:true,// 是否使用hls，默认不选择，如果此属性设置成true，则不能设置flashplayer:true,
           // chtrack: {
@@ -73,10 +81,10 @@ https://github.com/niandeng-ckplayer/chplayer
           // ], 
           // 提示点，words=提示点的内容，time=提示点对应的时间
           video: [
-            ['http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4', 'video/mp4', '中文标清', 0],
-            ['http://img.ksbbs.com/asset/Mon_1703/d0897b4e9ddd9a5.mp4', 'video/mp4', '中文高清', 0],
-            ['http://img.ksbbs.com/asset/Mon_1703/eb048d7839442d0.mp4', 'video/mp4', '英文高清', 0],
-            ['http://img.ksbbs.com/asset/Mon_1703/d30e02a5626c066.mp4', 'video/mp4', '英文超清', 0]
+            // ['http://img.ksbbs.com/asset/Mon_1703/05cacb4e02f9d9e.mp4', 'video/mp4', '中文标清', 0],
+            // ['http://img.ksbbs.com/asset/Mon_1703/d0897b4e9ddd9a5.mp4', 'video/mp4', '中文高清', 0],
+            // ['http://img.ksbbs.com/asset/Mon_1703/eb048d7839442d0.mp4', 'video/mp4', '英文高清', 0],
+            // ['http://img.ksbbs.com/asset/Mon_1703/d30e02a5626c066.mp4', 'video/mp4', '英文超清', 0]
           ]
           /* video:[
             ['http:// cdn.theoplayer.com/video/star_wars_episode_vii-the_force_awakens_official_comic-con_2015_reel_(2015)/index.m3u8','','标清',0],
@@ -89,9 +97,14 @@ https://github.com/niandeng-ckplayer/chplayer
       value: {
         default: ''
       },
-      poption: {
+      // 是否模态，即是否产生遮罩效果
+      modal: {
+        default: true
+      },
+      title: '',
+      opt: {
         default: function () {
-          // 
+          return {};
         }
       }
     },
@@ -102,23 +115,76 @@ https://github.com/niandeng-ckplayer/chplayer
             this.initPlayer();
           });
         }
+        if (val + '' === 'true') {
+          this.creatZz();
+        } else {
+          this.removeZz();
+        }
+      },
+      'opt.video': {
+        deep: true,
+        handler: function () {
+          this.changeVideo();
+        }
       }
     },
     computed: {
       //  
     },
     beforeDestroy: function () {
-      //  
+      this.removeZz();
     },
     mounted: function () {
-      // console.log(Chplayer);
+      this.creatZz();
     },
     methods: {
       initPlayer: function () {
-        console.log(window.chplayer);
         var Chplayer = window.chplayer;
         
-        this.player = new Chplayer(this.option);
+        this.player = new Chplayer(Object.assign(this.option, this.opt));
+      },
+      changeVideo: function (videoUrl) {
+        if (this.player) {
+          var Chplayer = window.chplayer;
+          var newVideoObject = Object.assign(this.option, this.opt);
+
+          if (this.player.playerType === 'html5video') {
+            if (/\.(flv|m3u8|f4v|rtmp)/g.test(JSON.stringify(videoUrl))) {
+              this.player.removeChild();
+              this.player = null;
+              this.player = new Chplayer();
+              this.player.embed(newVideoObject);
+            } else {
+              this.player.newVideo(newVideoObject);
+            }
+          // } else {
+          } else if (/\.(mp4|webm|ogg)/g.test(JSON.stringify(videoUrl))) {
+            this.player = null;
+            this.player = new Chplayer();
+            this.player.embed(newVideoObject);
+          } else {
+            this.player.newVideo(newVideoObject);
+          }
+          // }
+        }
+      },
+      clkClose: function () {
+        this.$emit('input', false);
+      },
+      creatZz: function () {
+        if (this.modal + '' === 'true' && this.value + '' === 'true') {
+          var dom = document.createElement('div');
+
+          dom.setAttribute('id', 'vdzz_' + this.id);
+          dom.setAttribute('class', 'center-hv');
+          dom.setAttribute('style', 'position: fixed;background-color: rgba(0, 0, 0, 0.1);z-index: ' + this.zIndex);
+          document.body.appendChild(dom);
+        }
+      },
+      removeZz: function () {
+        var dom = document.getElementById('vdzz_' + this.id);
+
+        dom && (document.body.removeChild(dom));
       }
     }
   };
@@ -134,8 +200,32 @@ https://github.com/niandeng-ckplayer/chplayer
     left: 0;
     width: 800px;
     height: 600px;
-    background-color: #fff;
+    background-color: #ddd;
     z-index: 3;
+
+    > header {
+      height: 45px;
+      border-bottom: solid 1px #ddd;
+
+      > span {
+        float: left;
+        margin-left: 10px;
+        line-height: 45px;
+      }
+
+      > .cicon-cross-cpt-chr {
+        float: right;
+        margin-top: 5px;
+        margin-right: 5px;
+        font-size: 34px;
+        color: #999;
+        // background-color: red;
+      }
+    }
+
+    > section {
+      height: calc(100% - 45px);
+    }
   }
 
   @keyframes zoomIn{0%{opacity:0;transform:scale3d(.3,.3,.3)}50%{opacity:1}}
