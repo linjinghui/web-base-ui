c032e43e3fc8bfbaccd89bc0fcd8e2eb0689b219
linjinghui <852530363@qq.com>
2018-12-04 12:11:54 +0800

    demo调整

--- example/demo_image_viewer.vue
+++ example/demo_image_viewer.vue
@@ -1,11 +1,10 @@
 <template>
   <div>
-    <cmp-image-viewer :opt="imgOption">
-      <!-- <ul>
-        <li><img src="http://www.w3school.com.cn/ui2017/compatible_firefox.png" alt=""></li>
-        <li><img src="https://ss3.baidu.com/-rVXeDTa2gU2pMbgoY3K/it/u=2192674209,2157056795&fm=202&mola=new&crop=v1" alt=""></li>
-      </ul> -->
-      <img src="http://www.w3school.com.cn/ui2017/compatible_firefox.png" alt="">
+    <cmp-image-viewer :opt="imgOption" v-if="imgs&&imgs.length>0">
+      <ul>
+        <li v-for="info in imgs"><img :src="info" alt=""></li>
+      </ul>
+      <!-- <img src="http://www.w3school.com.cn/ui2017/compatible_firefox.png" alt=""> -->
     </cmp-image-viewer>
   </div>
 </template>
@@ -17,6 +16,7 @@ export default {
   name: 'demoImageViewer',
   data: function () {
     return {
+      imgs: [],
       imgOption: {
         button: false,
         show: function () {
@@ -30,6 +30,11 @@ export default {
   },
   watch: {
     //
+  },
+  mounted: function () {
+    this.$nextTick(function () {
+      this.imgs = ['http://www.w3school.com.cn/ui2017/compatible_firefox.png', 'https://ss3.baidu.com/-rVXeDTa2gU2pMbgoY3K/it/u=2192674209,2157056795&fm=202&mola=new&crop=v1'];
+    });
   },
   methods: {
     //
