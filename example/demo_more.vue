085c3dbafbf909a377c322afbdadc339e03deba2
linjinghui <852530363@qq.com>
2019-06-24 17:37:27 +0800

    fix: 新增加载更多功能

--- example/demo_more.vue
+++ example/demo_more.vue
@@ -1,17 +1,22 @@
 <template>
-  <div style="position:relative;width:400px;height:300px;border:solid 1px;">
-    <cmp-more></cmp-more>
+  <div style="position:relative;width:400px;height:300px;border:solid 1px;overflow:auto;">
+    <ul>
+      <li v-for="(item) in arr" :key="item">item-{{item}}</li>
+    </ul>
+    <cmp-more :status="status" @callback="callback"></cmp-more>
   </div>
 </template>
 
 <script>
 import {More} from '../packages/index.js';
+import { setTimeout } from 'timers';
 
 export default {
   name: 'demoMore',
   data: function () {
     return {
-      //
+      status: 'more',
+      arr: []
     };
   },
   components: {
@@ -21,7 +26,19 @@ export default {
     //
   },
   methods: {
-    //
+    callback: function (data) {
+      var _this = this;
+      this.status = 'loading';
+      setTimeout(function () {
+        var _arr = [];
+        var i = _this.arr.length;
+        for (i = 0;i < 20;i++) {
+          _arr[_arr.length] = i;
+        }
+        _this.arr = _this.arr.concat(_arr);
+        _this.status = 'more';
+      }, 1500);
+    }
   }
 };
 </script>
--- packages/more/src/main.vue
+++ packages/more/src/main.vue
@@ -4,7 +4,7 @@
  -->
 
 <template>
-  <div class="wrap-more" :id="id">moren</div>
+  <p class="wrap-more" @click="clk" v-html="status==='more'?'加载更多':status==='loading'?'加载中...':'没有更多数据'"></p>
 </template>
 
 <script type="text/babel">
@@ -12,32 +12,67 @@
     name: 'More',
     data: function () {
       return {
-        id: 'more_' + new Date().getTime() + parseInt(Math.random() * 100)
+        id: 'more_' + new Date().getTime() + parseInt(Math.random() * 100),
+        timer: ''
       };
     },
     props: {
-      value: {
-        default: ''
+      status: {
+        // more | loading | nomore
+        default: 'more'
       }
     },
-    watch: {},
-    computed: {
-      // 
+    watch: {
+      status: function (val) {
+        if (val === 'more') {
+          this.clearListen();
+          this.addListen();
+        } else if (val === 'nomore') {
+          this.clearListen();
+        }
+      }
     },
     beforeDestroy: function () {
-      // 
+      this.clearListen();
     },
     mounted: function () {
-      // 
+      this.addListen();
     },
     methods: {
-      // 
+      // 监听wrap-more是否显示在屏幕中
+      addListen: function () {
+        var _this = this;
+
+        if (this.status === 'more') {
+          this.timer = setInterval(function () {
+            var dom = document.querySelector('.wrap-more');
+            // 元素顶端到可见区域顶端的距离
+            var top = dom.getBoundingClientRect().top;
+            // 浏览器可见区域高度。
+            var se = document.documentElement.clientHeight;
+
+            if (top <= se) {
+              _this.clearListen();
+              _this.$emit('callback');
+            }
+          }, 1000);
+        }
+      },
+      clearListen: function () {
+        this.timer && clearInterval(this.timer);
+      },
+      clk: function () {
+        if (this.status !== 'nomore') {
+          this.$emit('click');
+        }
+      }
     }
   };
 </script>
 
 <style scoped lang="scss">
   .wrap-more {
-    
+    padding: 10px;
+    text-align: center;
   }
 </style>
