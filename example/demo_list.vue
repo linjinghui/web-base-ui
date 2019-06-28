795bf0de806ece8da0c8803d84e3d2dec9417191
linjinghui <852530363@qq.com>
2019-06-28 15:17:36 +0800

    fix: list 组件

--- example/demo_list.vue
+++ example/demo_list.vue
@@ -1,21 +1,55 @@
 <template>
-  <div>
-    demo
+  <div style="width:300px;height:600px;border:solid 1px;">
+    <cmp-list :data="arr">
+      <dt slot="dt">标题</dt>
+      <dd slot="dd" slot-scope="props" class="arrow" v-if="props.index===0">单行带箭头</dd>
+      <dd slot="dd" slot-scope="props" v-else-if="props.index===1">
+        <div>左右等分</div>
+        <div>左右等分</div>
+      </dd>
+      <dd slot="dd" slot-scope="props" v-else-if="props.index===2">
+        <div class="text-img">
+          <p>上文、下图</p>
+          <img width="100%" src="http://cmstop.ndsww.com/a/10001/201904/ad3ea45820779176ce9657a422c12eee.jpeg">
+        </div>
+      </dd>
+      <dd slot="dd" slot-scope="props" v-else-if="props.index===3">
+        <div class="text-img">
+          <p class="text-eps">上文省略号、下图xxxxxxxxxxxxxxxxxxxxxxxxx</p>
+          <img width="100%" src="http://cmstop.ndsww.com/a/10001/201904/ad3ea45820779176ce9657a422c12eee.jpeg">
+        </div>
+      </dd>
+      <dd slot="dd" slot-scope="props" v-else-if="props.index===4">
+        <div class="text-img">
+          <p>上文、下三图 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</p>
+          <img width="30%" height="70px" src="http://cmstop.ndsww.com/a/10001/201904/ad3ea45820779176ce9657a422c12eee.jpeg">
+          <img width="30%" height="70px" src="http://cmstop.ndsww.com/a/10001/201904/ad3ea45820779176ce9657a422c12eee.jpeg">
+          <img width="30%" height="70px" src="http://cmstop.ndsww.com/a/10001/201904/ad3ea45820779176ce9657a422c12eee.jpeg">
+        </div>
+      </dd>
+      <dd slot="dd" slot-scope="props" v-else-if="props.index===5">
+        <div style="flex:80px;">
+          <img width="70px" height="70px" src="http://cmstop.ndsww.com/a/10001/201904/ad3ea45820779176ce9657a422c12eee.jpeg">
+        </div>
+        <div style="flex:calc(100% - 80px);text-align:left;">左图、右文 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</div>
+      </dd>
+      <dd slot="dd" slot-scope="props" v-else>{{props.item}}</dd>
+    </cmp-list>
   </div>
 </template>
 
 <script>
-import {Button} from '../packages/index.js';
+import {List} from '../packages/index.js';
 
 export default {
-  name: 'demoButton',
+  name: 'demoList',
   data: function () {
     return {
-      //
+      arr: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     };
   },
   components: {
-    'cmpButton': Button
+    'cmpList': List
   },
   watch: {
     //
--- packages/list/src/main.vue
+++ packages/list/src/main.vue
@@ -4,7 +4,10 @@
  -->
 
 <template>
-  <div class="wrap-list" :id="id">listn</div>
+  <ol class="wrap-list">
+    <slot name="dt"></slot>
+    <slot name="dd" v-for="(item,index) in data" :index="index" :item="item"></slot>
+  </ol>
 </template>
 
 <script type="text/babel">
@@ -16,8 +19,10 @@
       };
     },
     props: {
-      value: {
-        default: ''
+      data: {
+        default: function () {
+          return [];
+        }
       }
     },
     watch: {},
@@ -37,7 +42,97 @@
 </script>
 
 <style scoped lang="scss">
+  
   .wrap-list {
+    user-select: none;
+    background-color: #fff;
+
+    > dt, > dd {
+      position: relative;
+      display: flex;
+      padding-top: 8px;
+      padding-bottom: 8px;
+      padding-left: 10px;
+      padding-right: 10px;
+      width: 100%;
+      word-break: break-all;
+
+      > * {
+        flex: 1;
+      }
+      > *:nth-of-type(2) {
+				text-align: right;
+			}
+    }
+
+    > dt {
+      padding-top: 4px;
+      padding-bottom: 4px;
+    }
+
+    // 下划线
+    > dt:not(.noline):before, 
+		> dd:not(.noline):not(:last-of-type):before {
+      content: '';
+			position: absolute;
+			bottom: 0;
+      left: 0;
+      right: 0;
+      margin: auto;
+			width: calc(100% - 20px);
+			border-bottom: solid 1px #f3f3f3;
+    }
+
+    // 箭头
+    > dt.arrow,
+    > dd.arrow {
+			padding-right: 25px;
+		}
+		> dt.arrow:after,
+		> dd.arrow:after {
+			content: '';
+			position: absolute;
+			top: 0;
+			right: 12px;
+			bottom: 0;
+			margin: auto;
+			width: 8px;
+			height: 8px;
+			border: solid 1px #a3a3a3;
+			border-left: 0;
+			border-bottom: 0;
+			transform: rotate(45deg);
+    }
     
+    > dd:active {
+      background-color: #f4f4f4;
+    }
+
+    > dd {
+
+      img {
+        border-radius: 5px;
+      }
+      
+      // 上文、下大图
+      > div.text-img {
+        width: 100%;
+        font-size: 0;
+        > p {
+          line-height: 24px;
+          font-size: 14px;
+        }
+
+        > img {
+          display: inline-block;
+          margin-right: 5%;
+        }
+        > img:last-of-type {
+          margin-right: 0;
+        }
+        
+      }
+
+    }
   }
 </style>
