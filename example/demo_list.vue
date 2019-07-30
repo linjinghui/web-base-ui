f9b271661e75b72da42f9b1d62f608c395f67e7d
linjinghui <852530363@qq.com>
2019-07-30 17:37:45 +0800

    fix: list 表单调整

--- example/demo_list.vue
+++ example/demo_list.vue
@@ -1,39 +1,76 @@
 <template>
-  <div style="width:300px;height:600px;border:solid 1px;">
-    <cmp-list :data="arr">
+  <div>
+    <cmp-list :data="arr" style="width:300px;height:600px;border:solid 1px;overflow:auto;background-color: #fff;">
       <dt slot="dt">标题</dt>
-      <dd slot="dd" slot-scope="props" class="arrow" v-if="props.index===0">单行带箭头</dd>
-      <dd slot="dd" slot-scope="props" v-else-if="props.index===1">
-        <div>左右等分</div>
-        <div>左右等分</div>
-      </dd>
-      <dd slot="dd" slot-scope="props" v-else-if="props.index===2">
-        <div class="text-img">
+      <template slot="dds">
+        <dd>
+          <span class="flex-1">左右等分</span>
+          <span class="flex-1 txt-r">左右等分</span>
+        </dd>
+
+        <dd class="noline">
+          <span class="flex-1">无底边</span>
+        </dd>
+
+        <dd>
+          <span class="flex-1">单行带箭头</span>
+          <i class="arrow"></i>
+        </dd>
+
+        <dd>
           <p>上文、下图</p>
           <img width="100%" src="http://cmstop.ndsww.com/a/10001/201904/ad3ea45820779176ce9657a422c12eee.jpeg">
-        </div>
-      </dd>
-      <dd slot="dd" slot-scope="props" v-else-if="props.index===3">
-        <div class="text-img">
+        </dd>
+
+        <dd>
           <p class="text-eps">上文省略号、下图xxxxxxxxxxxxxxxxxxxxxxxxx</p>
           <img width="100%" src="http://cmstop.ndsww.com/a/10001/201904/ad3ea45820779176ce9657a422c12eee.jpeg">
-        </div>
-      </dd>
-      <dd slot="dd" slot-scope="props" v-else-if="props.index===4">
-        <div class="text-img">
+        </dd>
+
+        <dd>
           <p>上文、下三图 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</p>
           <img width="30%" height="70px" src="http://cmstop.ndsww.com/a/10001/201904/ad3ea45820779176ce9657a422c12eee.jpeg">
+          <img style="margin:0 5%;" width="30%" height="70px" src="http://cmstop.ndsww.com/a/10001/201904/ad3ea45820779176ce9657a422c12eee.jpeg">
           <img width="30%" height="70px" src="http://cmstop.ndsww.com/a/10001/201904/ad3ea45820779176ce9657a422c12eee.jpeg">
-          <img width="30%" height="70px" src="http://cmstop.ndsww.com/a/10001/201904/ad3ea45820779176ce9657a422c12eee.jpeg">
-        </div>
-      </dd>
-      <dd slot="dd" slot-scope="props" v-else-if="props.index===5">
-        <div style="flex:80px;">
+        </dd>
+
+        <dd>
           <img width="70px" height="70px" src="http://cmstop.ndsww.com/a/10001/201904/ad3ea45820779176ce9657a422c12eee.jpeg">
-        </div>
-        <div style="flex:calc(100% - 80px);text-align:left;">左图、右文 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</div>
-      </dd>
-      <dd slot="dd" slot-scope="props" v-else>{{props.item}}</dd>
+          <span class="flex-1" style="padding-left:10px;">左图、右文 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</span>
+        </dd>
+
+        <dd>
+          <span class="flex-1" style="padding-right:10px;">左文、右图 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</span>
+          <img width="70px" height="70px" src="http://cmstop.ndsww.com/a/10001/201904/ad3ea45820779176ce9657a422c12eee.jpeg">
+        </dd>
+
+        <dd>
+          <p>嘻嘻嘻嘻嘻嘻嘻</p>
+          <span>姓名</span>
+          <input class="flex-1" type="text">
+          <i class="arrow"></i>
+        </dd>
+
+        <dd>222</dd>
+      </template>
+    </cmp-list>
+
+    <cmp-list class="wx" :data="arr" style="margin-top:10px;width:300px;overflow:auto;background-color: #fff;">
+      <dt slot="dt">微信-设置</dt>
+      <template slot="dds">
+        <dd><span class="flex-1">新消息提醒</span><i class="arrow"></i></dd>
+        <dd><span class="flex-1">勿扰模式</span><i class="arrow"></i></dd>
+        <dd><span class="flex-1">手机号</span><span class="flex-1 txt-r">13599081111</span></dd>
+        <dd><span class="flex-1">用户名</span><input class="flex-1 txt-r" type="text" placeholder="请输入用户名"></dd>
+        <dd><span class="flex-1">用户名</span><input class="flex-1 txt-r" type="text" placeholder="请选择" readonly="readonly"><i class="arrow" style="color:#999;"></i></dd>
+        <dd>
+          <span class="flex-1">
+            <p>微信安全中心</p>
+            <p style="color:#999;font-size:12px;">如果你遇到微信被盗，无法登录等问题，可以前往安全中心</p>
+          </span>
+          <i class="arrow"></i>
+        </dd>
+      </template>
     </cmp-list>
   </div>
 </template>
@@ -60,6 +97,17 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  dt, dd {
+    padding-left: 10px;
+    padding-right: 10px;
+  }
 
+  .wx {
+    > dt:not(.noline):after, 
+		> dd:not(.noline):not(:last-of-type):after {
+      left: 10px!important;
+      width: calc(100% - 10px)!important;
+    }
+  }
 </style>
--- packages/list/src/main.vue
+++ packages/list/src/main.vue
@@ -6,133 +6,90 @@
 <template>
   <ol class="wrap-list">
     <slot name="dt"></slot>
-    <slot name="dd" v-for="(item,index) in data" :index="index" :item="item"></slot>
+    <slot name="dds"></slot>
   </ol>
 </template>
-
-<script type="text/babel">
-  export default {
-    name: 'List',
-    data: function () {
-      return {
-        id: 'list_' + new Date().getTime() + parseInt(Math.random() * 100)
-      };
-    },
-    props: {
-      data: {
-        default: function () {
-          return [];
-        }
-      }
-    },
-    watch: {},
-    computed: {
-      // 
-    },
-    beforeDestroy: function () {
-      // 
-    },
-    mounted: function () {
-      // 
-    },
-    methods: {
-      // 
-    }
-  };
-</script>
 
 <style scoped lang="scss">
   
   .wrap-list {
+    border: solid 1px #f3f3f3;
+    border-left: 0;
+    border-right: 0;
     user-select: none;
-    background-color: #fff;
 
     > dt, > dd {
       position: relative;
       display: flex;
+      flex-shrink: 0;
+      flex-wrap: wrap;
+      align-items: center;
       padding-top: 8px;
       padding-bottom: 8px;
-      padding-left: 10px;
-      padding-right: 10px;
       width: 100%;
+      line-height: 22px;
       word-break: break-all;
 
-      > * {
+      > p {
+        width: 100%;
+      }
+
+      > .flex-1 {
         flex: 1;
       }
-      > *:nth-of-type(2) {
-				text-align: right;
-			}
-    }
 
-    > dt {
-      padding-top: 4px;
-      padding-bottom: 4px;
+      // 输入框
+      > input {
+        border: 0;
+        padding: 0;
+        background-color: transparent;
+      }
+
+      // 文本向右
+      .txt-r {
+        text-align: right;
+      }
+
+      // 箭头
+      .arrow {
+        position: relative;
+        display: inline-block;
+        width: 12px;
+        height: 12px;
+      }
+      .arrow:after {
+        content: '';
+        position: absolute;
+        top: 0;
+        right: 4px;
+        bottom: 0;
+        left: 0;
+        margin: auto;
+        width: 6px;
+        height: 6px;
+        color: inherit;
+        border: solid 1px;
+        border-left: 0;
+        border-bottom: 0;
+        transform: rotate(45deg);
+      }
     }
 
     // 下划线
-    > dt:not(.noline):before, 
-		> dd:not(.noline):not(:last-of-type):before {
+    > dt:not(.noline):after, 
+		> dd:not(.noline):not(:last-of-type):after {
       content: '';
-			position: absolute;
-			bottom: 0;
+      position: absolute;
       left: 0;
-      right: 0;
-      margin: auto;
-			width: calc(100% - 20px);
+      bottom: 0;
+      width: 100%;
+      height: 1px;
 			border-bottom: solid 1px #f3f3f3;
-    }
-
-    // 箭头
-    > dt.arrow,
-    > dd.arrow {
-			padding-right: 25px;
-		}
-		> dt.arrow:after,
-		> dd.arrow:after {
-			content: '';
-			position: absolute;
-			top: 0;
-			right: 12px;
-			bottom: 0;
-			margin: auto;
-			width: 8px;
-			height: 8px;
-			border: solid 1px #a3a3a3;
-			border-left: 0;
-			border-bottom: 0;
-			transform: rotate(45deg);
     }
     
     > dd:active {
       background-color: #f4f4f4;
     }
 
-    > dd {
-
-      img {
-        border-radius: 5px;
-      }
-      
-      // 上文、下大图
-      > div.text-img {
-        width: 100%;
-        font-size: 0;
-        > p {
-          line-height: 24px;
-          font-size: 14px;
-        }
-
-        > img {
-          display: inline-block;
-          margin-right: 5%;
-        }
-        > img:last-of-type {
-          margin-right: 0;
-        }
-        
-      }
-
-    }
   }
 </style>
